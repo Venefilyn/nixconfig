f9d1fcacf488de7dd06255fb21b73961ee63b4b7
Freya Gustavsson <freya@spytec.se>
2024-07-09 14:11:15 +0200

    Added git aliases

--- sabre/spytec.nix
+++ sabre/spytec.nix
@@ -4,12 +4,16 @@
     programs.git = {
         enable = true;
         aliases = {
+            prune-all = "!git remote | xargs -n 1 git remote prune";
+            pullr = "pull --rebase";
+            ll = "log --oneline";
+            pushfwl = "push --force-with-lease";
             pushall = "!git remote | xargs -L1 git push --all";
             graph = "log --decorate --oneline --graph";
             add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
         };
         userName = "Freya Gustavsson";
-        userEmail = "freya@spytec.se";
+        userEmail = "freya@venefilyn.se";
         extraConfig = {
             feature.manyFiles = true;
             init.defaultBranch = "main";
