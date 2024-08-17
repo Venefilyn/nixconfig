e4afd608e521f48a8b8bd48193fd2558c3bd0146
Freya Gustavsson <freya@spytec.se>
2024-08-17 03:12:21 +0200

    Re-added properties of zsh

--- shell.nix
+++ shell.nix
@@ -10,12 +10,12 @@
     theme = "TwoDark";
   };
   # zsh
-  # programs.zsh.profileExtra = ''
-  #   [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
-  #   export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
-  # '';
-  # programs.zsh.enableCompletion = true;
-  # programs.zsh.enable = true;
+  programs.zsh.profileExtra = ''
+    [ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source  ~/.nix-profile/etc/profile.d/nix.sh
+    export XCURSOR_PATH=$XCURSOR_PATH:/usr/share/icons:~/.local/share/icons:~/.icons:~/.nix-profile/share/icons
+  '';
+  programs.zsh.enableCompletion = true;
+  programs.zsh.enable = true;
   programs.fish.enable = true;
   programs.neovim = {
     enable = true;
