23c1971c11a666462e92672d22a63d7ec3d5a54c
Freya Gustavsson <freya@venefilyn.se>
2024-09-20 16:48:52 +0200

    Added git aliases to frieren

--- frieren/spytec.nix
+++ frieren/spytec.nix
@@ -1,10 +1,13 @@
 { pkgs, misc, ... }: {
-  # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
     home.username = "spytec";
     home.homeDirectory = "/home/spytec";
     programs.git = {
         enable = true;
         aliases = {
+            prune-all = "!git remote | xargs -n 1 git remote prune";
+            pullr = "pull --rebase";
+            ll = "log --oneline";
+            pushfwl = "push --force-with-lease";
             pushall = "!git remote | xargs -L1 git push --all";
             graph = "log --decorate --oneline --graph";
             add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
@@ -18,7 +21,7 @@
         };
 
         signing = {
-            key = "~/.ssh/id_ed25519";
+            key = "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESuE0f0GVIgkMYwR/0npxQGq+XzZxjZ4Fz9ONgpdExr freya@venefilyn.se";
             signByDefault = builtins.stringLength "~/.ssh/id_ed25519" > 0;
         };
 
