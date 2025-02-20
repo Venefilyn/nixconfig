7f28010927b6c959dbba1e77afd356c76b72c3c4
Freya Gustavsson <freya@venefilyn.se>
2025-02-21 00:57:24 +0100

    Fixed unmount issue in `dlsrcam`

--- aliases.nix
+++ aliases.nix
@@ -19,6 +19,6 @@
     osm_mp4 = "ffmpeg -framerate 1 -pattern_type glob -i '*.png' -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4";
     osm_gif = "ffmpeg -i out.mp4 -vf \"fps=1,scale=1920:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 out.gif";
     osm_progress = "osm_mp4 && osm_gif";
-    dlsrcam = "gio mount -s gphoto2 && gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0";
+    dlsrcam = "gio mount -s gphoto2 & wait $last_pid && gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0";
   };
 }
